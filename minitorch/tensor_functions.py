88b1b3a35f69f6e0200ea29ca18c974827a63d73
axxx03 <869974612@qq.com>
2025-03-25 14:10:30 +0000

    init

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -1,10 +1,13 @@
-"""Implementation of the autodifferentiation Functions for Tensor."""
+"""
+Implementation of the autodifferentiation Functions for Tensor.
+"""
 
 from __future__ import annotations
 
 import random
 from typing import TYPE_CHECKING
 
+import copy
 import numpy as np
 
 import minitorch
@@ -20,8 +23,8 @@ if TYPE_CHECKING:
     from .tensor_data import UserIndex, UserShape
 
 
-def wrap_tuple(x: Any) -> tuple:  # type: ignore
-    """Turn a possible value into a tuple"""
+def wrap_tuple(x):  # type: ignore
+    "Turn a possible value into a tuple"
     if isinstance(x, tuple):
         return x
     return (x,)
@@ -39,7 +42,6 @@ class Function:
 
     @classmethod
     def apply(cls, *vals: Tensor) -> Tensor:
-        """Call the forward function and track history"""
         raw_vals = []
         need_grad = False
         for v in vals:
@@ -95,17 +97,136 @@ class Add(Function):
         return grad_output, grad_output
 
 
+class Mul(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, b: Tensor) -> Tensor:
+        ctx.save_for_backward(a, b)
+        return a.f.mul_zip(a, b)
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
+        (a, b) = ctx.saved_values
+        return  grad_output.f.mul_zip(grad_output, b), \
+                grad_output.f.mul_zip(grad_output, a)
+
+class Sigmoid(Function):
+    @staticmethod
+    def forward(ctx: Context, t1: Tensor) -> Tensor:
+        res = t1.f.sigmoid_map(t1)
+        ctx.save_for_backward(res)
+        return res
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tensor:
+        (b,) = ctx.saved_values
+        # backward = grad_output * b * (1 - b)
+        
+        oneMinusB = grad_output.f.add_zip(
+            tensor([1.0]), 
+            grad_output.f.neg_map(b)
+        )
+
+        BPlusOneMinusB = grad_output.f.mul_zip(b, oneMinusB)
+        
+        return grad_output.f.mul_zip(grad_output, BPlusOneMinusB)
+
+class ReLU(Function):
+    @staticmethod
+    def forward(ctx: Context, t1: Tensor) -> Tensor:
+        ctx.save_for_backward(t1)
+        return t1.f.relu_map(t1)
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tensor:
+        (t1,) = ctx.saved_values
+        return grad_output.f.relu_back_zip(t1, grad_output)
+
+class Log(Function):
+    @staticmethod
+    def forward(ctx: Context, t1: Tensor) -> Tensor:
+        ctx.save_for_backward(t1)
+        return t1.f.log_map(t1)
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tensor:
+        (t1,) = ctx.saved_values
+        return grad_output.f.log_back_zip(t1, grad_output)
+
+class Exp(Function):
+    @staticmethod
+    def forward(ctx: Context, t1: Tensor) -> Tensor:
+        res = t1.f.exp_map(t1)
+        ctx.save_for_backward(res)
+        return res
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tensor:
+        (t1,) = ctx.saved_values
+        return grad_output.f.mul_zip(t1, grad_output)
+
+class Sum(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, dim: Tensor) -> Tensor:
+        ctx.save_for_backward(a.shape, dim)
+        return a.f.add_reduce(a, int(dim.item()))
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, float]:
+        a_shape, dim = ctx.saved_values
+        return grad_output, 0.0
+
+
 class All(Function):
     @staticmethod
     def forward(ctx: Context, a: Tensor, dim: Tensor) -> Tensor:
-        """Return 1 if all are true"""
         if dim is not None:
             return a.f.mul_reduce(a, int(dim.item()))
         else:
             return a.f.mul_reduce(a.contiguous().view(int(operators.prod(a.shape))), 0)
 
 
+class LT(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, b: Tensor) -> Tensor:
+        return a.f.lt_zip(a, b)
 
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
+        return 0.0, 0.0
+
+class EQ(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, b: Tensor) -> Tensor:
+        return a.f.eq_zip(a, b)
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
+        return 0.0, 0.0
+
+class IsClose(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, b: Tensor) -> Tensor:
+        return a.f.is_close_zip(a, b)
+
+class Permute(Function):
+    @staticmethod
+    def forward(ctx: Context, a: Tensor, order: Tensor) -> Tensor:
+        order_ = [int(order[i]) for i in range(order.shape[0])]
+        ctx.save_for_backward(order_)
+
+        return a._new(a._tensor.permute(*order_))
+
+    # # 没弄懂转置的反向传播
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, float]:
+        (order_,) = ctx.saved_values
+        r_order = copy.deepcopy(order_)
+
+        for index,i in enumerate(order_):
+            r_order[i] = index
+
+        return grad_output._new(grad_output._tensor.permute(*r_order)),0.0
+        
 
 class View(Function):
     @staticmethod
@@ -119,7 +240,6 @@ class View(Function):
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, float]:
-        """Matrix Multiply backward (module 3)"""
         (original,) = ctx.saved_values
         return (
             minitorch.Tensor.make(
@@ -132,25 +252,21 @@ class View(Function):
 class Copy(Function):
     @staticmethod
     def forward(ctx: Context, a: Tensor) -> Tensor:
-        """Id function makes contiguous"""
         return a.f.id_map(a)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> Tensor:
-        """Undo"""
         return grad_output
 
 
 class MatMul(Function):
     @staticmethod
     def forward(ctx: Context, t1: Tensor, t2: Tensor) -> Tensor:
-        """Matrix Multiply Forward (module 3)"""
         ctx.save_for_backward(t1, t2)
         return t1.f.matrix_multiply(t1, t2)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
-        """Matrix Multiply backward (module 3)"""
         t1, t2 = ctx.saved_values
 
         def transpose(a: Tensor) -> Tensor:
@@ -166,20 +282,18 @@ class MatMul(Function):
 
 # Helpers for Constructing tensors
 def zeros(shape: UserShape, backend: TensorBackend = SimpleBackend) -> Tensor:
-    """Produce a zero tensor of size `shape`.
+    """
+    Produce a zero tensor of size `shape`.
 
     Args:
-    ----
         shape : shape of tensor
         backend : tensor backend
 
     Returns:
-    -------
         new tensor
-
     """
     return minitorch.Tensor.make(
-        [0.0] * int(operators.prod(shape)), shape, backend=backend
+        [0] * int(operators.prod(shape)), shape, backend=backend
     )
 
 
@@ -188,18 +302,16 @@ def rand(
     backend: TensorBackend = SimpleBackend,
     requires_grad: bool = False,
 ) -> Tensor:
-    """Produce a random tensor of size `shape`.
+    """
+    Produce a random tensor of size `shape`.
 
     Args:
-    ----
         shape : shape of tensor
         backend : tensor backend
         requires_grad : turn on autodifferentiation
 
     Returns:
-    -------
         :class:`Tensor` : new tensor
-
     """
     vals = [random.random() for _ in range(int(operators.prod(shape)))]
     tensor = minitorch.Tensor.make(vals, shape, backend=backend)
@@ -213,19 +325,17 @@ def _tensor(
     backend: TensorBackend = SimpleBackend,
     requires_grad: bool = False,
 ) -> Tensor:
-    """Produce a tensor with data ls and shape `shape`.
+    """
+    Produce a tensor with data ls and shape `shape`.
 
     Args:
-    ----
         ls: data for tensor
         shape: shape of tensor
         backend: tensor backend
         requires_grad: turn on autodifferentiation
 
     Returns:
-    -------
         new tensor
-
     """
     tensor = minitorch.Tensor.make(ls, shape, backend=backend)
     tensor.requires_grad_(requires_grad)
@@ -235,18 +345,16 @@ def _tensor(
 def tensor(
     ls: Any, backend: TensorBackend = SimpleBackend, requires_grad: bool = False
 ) -> Tensor:
-    """Produce a tensor with data and shape from ls
+    """
+    Produce a tensor with data and shape from ls
 
     Args:
-    ----
         ls: data for tensor
         backend : tensor backend
         requires_grad : turn on autodifferentiation
 
     Returns:
-    -------
         :class:`Tensor` : new tensor
-
     """
 
     def shape(ls: Any) -> List[int]:
@@ -283,7 +391,6 @@ def grad_central_difference(
 
 
 def grad_check(f: Any, *vals: Tensor) -> None:
-    """Check whether autodiff matches central difference."""
     for x in vals:
         x.requires_grad_(True)
         x.zero_grad_()
---
+++ run_test.sh
@@ -0,0 +1,49 @@
+# streamlit run project/app.py -- 0
+# streamlit run project/app.py -- 1
+# streamlit run project/app.py -- 2
+
+# 学习攻略： https://dezeming.top/wp-content/uploads/2022/02/MiniTorch-%E5%AD%A6%E4%B9%A0%E5%85%A8%E6%94%BB%E7%95%A5.pdf
+
+rm -rf log/test.log
+
+# echo "******** Task 0.1 ********" >> log/test.log
+# pytest -m task0_1 >> log/test.log 2>&1
+
+# echo "******** Task 0.2 ********" >> log/test.log
+# pytest -m task0_2 >> log/test.log 2>&1
+
+# echo "******** Task 0.3 ********" >> log/test.log
+# pytest -m task0_3 >> log/test.log 2>&1
+
+# echo "******** Task 0.4 ********" >> log/test.log
+# pytest -m task0_4 >> log/test.log 2>&1
+
+# echo "******** Task 0.5 ********" >> log/test.log
+# pytest -m task0_5 >> log/test.log 2>&1
+
+# echo "******** Task 1.1 ********" >> log/test.log
+# pytest -m task1_1 >> log/test.log 2>&1
+
+# echo "******** Task 1.2 ********" >> log/test.log
+# pytest -m task1_2 >> log/test.log 2>&1
+
+# echo "******** Task 1.3 ********" >> log/test.log
+# pytest -m task1_3 >> log/test.log 2>&1
+
+# echo "******** Task 1.4 ********" >> log/test.log
+# pytest -m task1_4 >> log/test.log 2>&1
+
+# echo "******** Task 1.5 ********" >> log/test.log
+# pytest -m task1_5 >> log/test.log 2>&1
+
+# echo "******** Task 2.1 ********" >> log/test.log
+# pytest -m task2_1 >> log/test.log 2>&1
+
+# echo "******** Task 2.2 ********" >> log/test.log
+# pytest -m task2_2 >> log/test.log 2>&1
+
+# echo "******** Task 2.3 ********" >> log/test.log
+# pytest -m task2_3 >> log/test.log 2>&1
+
+echo "******** Task 2.4 ********" >> log/test.log
+pytest -m task2_4 >> log/test.log 2>&1
